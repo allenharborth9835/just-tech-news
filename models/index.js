1dd56f75352cee4fd6289f39364a06a0142fd6d4
Harborth, Allen <allenharborth9835@gmail.com>
2021-09-14 07:09:29 -0500

    fix

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const User = require('./User.js');
-const Post = require('./Post.js');
-const Vote = require('./Vote.js');
-const Comment = require('./Comment.js')
+const User = require('./User');
+const Post = require('./Post');
+const Vote = require('./Vote');
+const Comment = require('./Comment')
 
 
 //create associations
